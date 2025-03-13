73ebbfac0c23b921581bba05f6fcc2a7bc74963d
Haxziel <gokussejsb@gmail.com>
2025-03-13 00:58:23 -0600

    Cambios obligatorios

--- ino/src/main.ts
+++ ino/src/main.ts
@@ -1,8 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker'; // Importar Service Worker
 import { FinanzasService } from './app/services/finanzas.service';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), FinanzasService], // Asegurar que el servicio es un provider
+  providers: [
+    provideHttpClient(),
+    FinanzasService,
+    provideServiceWorker('ngsw-worker.js', { enabled: true }) // Siempre habilitado
+  ],
 }).catch((err) => console.error(err));
