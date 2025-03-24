a2cd1ef482e807138e2b8d948b930d09d1e75405
Haxziel <gokussejsb@gmail.com>
2025-03-24 02:13:56 -0600

    1.0

--- ino/src/app/app.component.ts
+++ ino/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FinanzasService } from './services/finanzas.service';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 @Component({
   selector: 'app-root',
